94ff3ce4f8edb241d261de794259672225487edd
Wes Johnson <swj@wesquire.ca>
2013-10-02 20:58:39 -0400

    refactor jqueryui

--- client/datepicker.js
+++ client/datepicker.js
@@ -1,28 +1,22 @@
-Template.datepicker.created = function() {
-    Meteor.Loader.loadCss('/lib/jquery-ui/jquery-ui-1.10.3.custom.min.css');
-    Meteor.Loader.loadJs('/lib/jquery-ui/jquery-ui-1.10.3.custom.min.js', function() {
-        Session.set('hasJqueryUI',true);
-    });
-};
-
 Template.datepicker.rendered = function() {
     
     var that = this;
     
     if(this.data && this.data.classn) {
-        Session.whenTrue('hasJqueryUI', function() {
-            var options = {
-                dateFormat: "yy/m/d"
-            };
-            if(that.data.classn=="datetimepicker") {
-                _.extend(options, {
-                    timeFormat: "h:mm tt",
-                    stepMinute: 5
-                });
-            }
-            
-            $('.'+that.data.classn)[that.data.classn](options);
-        });
+        var options = {
+            dateFormat: "yy/m/d"
+        };
+        
+        $('.'+that.data.classn)[that.data.classn]('destroy');
+        
+        if(that.data.classn=="datetimepicker") {
+            _.extend(options, {
+                timeFormat: "h:mm tt",
+                stepMinute: 5
+            });
+        }
+        
+        $('.'+that.data.classn)[that.data.classn](options);
     }
 
 };
